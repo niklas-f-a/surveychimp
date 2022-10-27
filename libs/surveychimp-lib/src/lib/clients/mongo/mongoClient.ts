3ec493296809bf15d32fd264c97198c92acb06bd
Niklas Andersson <niklas.andersson@iths.se>
2022-10-27 17:30:29 +0200

    Add env variable

--- libs/surveychimp-lib/src/lib/clients/mongo/mongoClient.ts
+++ libs/surveychimp-lib/src/lib/clients/mongo/mongoClient.ts
@@ -2,6 +2,9 @@
 import mongoose from 'mongoose';
 
 export const connect = async (): Promise<typeof mongoose> => {
-    const connectionString = "mongodb+srv://jonatan:123qweasd@cluster0.otsv5.mongodb.net/?retryWrites=true&w=majority";
+    const connectionString = process.env.DB_URI;
+    if(typeof connectionString !== 'string'){
+        throw Error('500')
+    }
     return mongoose.connect(connectionString);
 }
