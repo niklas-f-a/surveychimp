227c535c8d9f250d852c6e1b1b24d6cb1baec60e
Damian A <damian.amalraj@hotmail.com>
2022-11-15 09:32:14 +0100

    Init sentry

--- apps/surveychimp-survey-spa/src/app/app.tsx
+++ apps/surveychimp-survey-spa/src/app/app.tsx
@@ -1,33 +1,21 @@
-
 import styled from 'styled-components';
 import SurveyPage from './pages/SurveyPage';
 
 import { Route, Routes } from 'react-router-dom';
 
-
-
 const StyledApp = styled.div`
-    // Your style here
+  // Your style here
 `;
 
-
 export function App() {
-
   return (
     <StyledApp>
       <Routes>
-        <Route
-          path="/survey/:surveyId"
-          element={<SurveyPage />}
-        />
-        <Route
-          path="/"
-          element={<div>Du är på förstasidan</div>} />
+        <Route path="/survey/:surveyId" element={<SurveyPage />} />
+        <Route path="/" element={<div>Du är på förstasidan</div>} />
       </Routes>
     </StyledApp>
   );
-
 }
-
 
 export default App;
--- apps/surveychimp-survey-spa/src/app/pages/SurveyPage.tsx
+++ apps/surveychimp-survey-spa/src/app/pages/SurveyPage.tsx
@@ -4,24 +4,25 @@ import getSurveyById from '../../api/getSurveyById';
 import { ISurvey } from '@surveychimp/surveychimp-lib';
 
 const SurveyPage = () => {
-    const { surveyId } = useParams();
-    const [survey, setSurvey] = useState<ISurvey>()
+  const { surveyId } = useParams();
+  const [survey, setSurvey] = useState<ISurvey>();
 
-    useEffect(() => {
-        if (surveyId) {
-            getSurveyById(surveyId).then(survey => {
-                setSurvey(survey);
-            })
-        } else {
-            //Visa upp att id saknas
-        }
-    }, [])
+  useEffect(() => {
+    if (surveyId) {
+      getSurveyById(surveyId).then((survey) => {
+        setSurvey(survey);
+      });
+    } else {
+      //Visa upp att id saknas
+    }
+  }, []);
 
-
-    return (<>
-        <h1>Survey</h1>
-        <p>{survey?.recipient.name}</p>
-    </>)
-}
+  return (
+    <>
+      <h1>Survey</h1>
+      <p>{survey?.recipient.name}</p>
+    </>
+  );
+};
 
 export default SurveyPage;
