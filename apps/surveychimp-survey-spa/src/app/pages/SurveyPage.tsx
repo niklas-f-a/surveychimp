efa795ae369a34fff59534f6401476ee69c02979
Damian A <damian.amalraj@hotmail.com>
2022-11-15 12:22:27 +0100

    Add fake crashs

--- apps/surveychimp-survey-spa/src/app/pages/SurveyPage.tsx
+++ apps/surveychimp-survey-spa/src/app/pages/SurveyPage.tsx
@@ -8,30 +8,32 @@ const SurveyPage = () => {
   const { surveyId } = useParams();
   const [survey, setSurvey] = useState<ISurvey>();
 
+  useEffect(() => {
+    if (surveyId) {
+      getSurveyById(surveyId).then((survey) => {
+        setSurvey(survey);
+      });
+    } else {
+      //Visa upp att id saknas
+    }
+  }, []);
+  const ratingChanged = (newRating: number) => {
+    console.log(newRating);
+    throw new Error('Fake crash');
+  };
 
-    useEffect(() => {
-        if (surveyId) {
-            getSurveyById(surveyId).then(survey => {
-                setSurvey(survey);
-            })
-        } else {
-            //Visa upp att id saknas
-        }
-    }, [])
-    const ratingChanged = (newRating: number) => {
-        console.log(newRating)
-      }
-
-    return (<>
-    <h1>Survey</h1>
-    <p>{survey?.recipient?.name}</p>
-    <ReactStars
+  return (
+    <>
+      <h1>Survey</h1>
+      <p>{survey?.recipient?.name}</p>
+      <ReactStars
         count={5}
         onChange={ratingChanged}
         size={24}
-        color2={'#ffd700'} />
-    </>)  
-}
+        color2={'#ffd700'}
+      />
+    </>
+  );
+};
 
 export default SurveyPage;
-
